aa9129fead08fe690f1eeb9c445f770217f27768
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-10 16:43:41 +0530

    instance practice in instance-practice

--- instance-launch/launch.sh
+++ instance-launch/launch.sh
@@ -6,3 +6,6 @@ if [ -z "${component}" ]; then
   echo "Component is Needed"
   exit 1
 fi
+aws ec2 run-instances --launch-template  LaunchTemplateId=lt-0cff640d7e3d4dbdf,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}]"
+
+
