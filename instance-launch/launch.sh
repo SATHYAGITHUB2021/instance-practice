f5a4dcd089b2ec5055622f214c5f0fd270191d97
SATHYANARAYANA.JOKU <sathyanarayana.joku14@gmail.com>
2021-06-10 17:19:51 +0530

    instance practice in instance-practice

--- instance-launch/launch.sh
+++ instance-launch/launch.sh
@@ -15,6 +15,11 @@ if [ "${INSTANCE_STATE}" = "running" ]; then
   exit 0
 fi
 
+if [ "${INSTANCE_STATE}" = "stopped" ]; then
+  echo "Instance Already Exists!!"
+  exit 0
+fi
+
 
 aws ec2 run-instances --launch-template  LaunchTemplateId=lt-0cff640d7e3d4dbdf,Version=2 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
 
